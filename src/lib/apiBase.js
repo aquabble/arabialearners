d8b50bad81b12a2a939bdb9af25a11982c20f092
aquabble <mcdaniel.t.jordan@gmail.com>
2025-09-02 08:36:51 -0700

    Delete src directory

--- src/lib/apiBase.js
+++
@@ -1,2 +0,0 @@
-// src/lib/apiBase.js
-export const API_BASE = import.meta.env.VITE_API_BASE || '';
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import './styles/ui.css'
-import './styles/responsive.css'
-import './styles/responsive.additions.css'
-import './styles/logo-dropdown.sleek.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-createRoot(document.getElementById('root')).render(<App />)
