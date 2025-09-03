9cea73593bee2f2e80c33f7990572206d1f7ea74
aquabble <mcdaniel.t.jordan@gmail.com>
2025-09-03 10:45:52 -0700

    Delete api directory

--- api/_json.js
+++
@@ -1,6 +0,0 @@
-export function json(obj, status=200, extra={}) {
-  return new Response(JSON.stringify(obj), {
-    status,
-    headers: { "content-type": "application/json; charset=utf-8", ...extra }
-  });
-}
--- api/sentence.js
+++
@@ -1,4 +0,0 @@
-// Thin wrapper (kept simple for Edge)
-import originalHandler from "./sentence.inner.js";
-export const config = { runtime: "edge" };
-export default async function handler(req){ return originalHandler(req); }
