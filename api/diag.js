da820d748dac717d8996598c0e1e3e2e59e52efd
aquabble <mcdaniel.t.jordan@gmail.com>
2025-09-22 11:12:02 -0700

    Delete api directory

--- api/diag.js
+++
@@ -1,9 +0,0 @@
-export const config = { runtime: "nodejs" }
-export default (req,res) => {
-  res.setHeader('Access-Control-Allow-Origin','*')
-  res.status(200).json({
-    ok:true,
-    node: process.version,
-    env: Object.keys(process.env).filter(k=>k.startsWith('VITE_')||k.startsWith('OPENAI_'))
-  })
-}
